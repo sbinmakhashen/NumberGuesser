f842b955c1dd385d67a6b43abf78e5bf6479c0eb
Salem <nlsabm@yahoo.com>
2021-03-08 00:51:51 -0500

    debuging and play agian feature

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ Game function or rules:
 // Game vars
 let min = 1,
   max = 10,
-  winningNum = 5,
+  winningNum = generateRandomNum(min, max),
   guessesLeft = 3;
 
 // get Ui elements
@@ -25,6 +25,13 @@ const game = document.getElementById('game'),
 // make min and max dynamic
 minNum.textContent = min;
 maxNum.textContent = max;
+
+// play again
+game.addEventListener('mousedown', function (e) {
+  if (e.target.className === 'play-again') {
+    window.location.reload();
+  }
+});
 
 // listen for the submit button
 guessBtn.addEventListener('click', function () {
@@ -42,13 +49,43 @@ guessBtn.addEventListener('click', function () {
     // make input border and message green 
     guessInput.style.borderColor = 'green';
     message.style.color = 'green';
+    // after won guessbtn text changes from 'submit' into 'play again' 
+    guessBtn.value = 'Play Again';
+    // adding a class in guessBtn to use for play agin
+    guessBtn.className = 'play-again';
+    // coloring btn
+    guessBtn.style.borderColor = 'green';
   } else { //when the player enters a wrong number
     guessesLeft -= 1;
-    if (guessesLeft == 0) {
-
+    if (guessesLeft === 0) {
+      // The player loses
+      eMessage(`Game Over! you have lost, the correct number was ${winningNum}`);
+      // disable the input
+      guessInput.disabled = true;
+      // turn input border and message into red
+      guessInput.style.borderColor = 'red';
+      message.style.color = 'red';
+      // after a loss guessbtn text changes from 'submit' into 'play again'
+      guessBtn.value = 'Play Again';
+      // adding a class in guessBtn to use for play agin
+      guessBtn.className = 'play-again';
+      // coloring btn
+      guessBtn.style.borderColor = 'red';
+    } else { //the game continues
+      eMessage(`${guess} is incorrect, now you have ${guessesLeft} guesses left`);
+      // clearing the input to allow user to enter the remaining guesses
+      guessInput.value = '';
+      // make input border and text red
+      guessInput.style.borderColor = 'red';
+      message.style.color = 'red';
     }
   }
 });
+
+// Generate random numbers
+function generateRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 // set message
 function eMessage(msg) {
   message.textContent = msg;
